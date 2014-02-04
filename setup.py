11ea0a04cfa079a0c9d350d3b724f75623caba84
Kevin Morey <kevin.morey@gmail.com>
2014-02-04 15:00:54 -0600

    Initial crack at a setup.py file that will work with pip
    Conflicts:
    	setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,21 @@ setup(
     packages=["libkeepass"],
     author="Lukas Koell",
     author_email="phpwutz@gmail.com",
-    description="A library to access keepass v3 (keepass1) and v4 (keepass2) files",
+    description="A library to access KeePass 1.x/KeePassX (v3) and KeePass "
+                "2.x (v4) files",
     license="GPL",
     keywords="keepass library",
     url="https://github.com/phpwutz/libkeepass",  # project home page, if any
-    install_requires=["lxml>=3.2.1", "nose>=1.3.0", "pycrypto>=2.6.1"],
-    test_suite="tests"
+    test_suite="tests",
+    packages=find_packages(),
+    install_requires=[
+        "lxml>=3.2.1",
+        "nose>=1.3.0",
+        "pycrypto>=2.6.1"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+    ]
 )
