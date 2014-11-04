56763857288cf5475757bce6bd12eb279babb3b2
George Schizas <gschizas@gmail.com>
2014-11-04 10:34:25 +0200

    PEP8 Formatting

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-import sys, os
+import os
 import sys
 import unittest
 
-import libkeepass, libkeepass.common, libkeepass.kdb4, libkeepass.kdb3
+import libkeepass
+import libkeepass.common
+import libkeepass.kdb4
+import libkeepass.kdb3
+
 
 sys.path.append(os.path.abspath("."))
 sys.path.append(os.path.abspath(".."))
@@ -11,29 +15,30 @@ sys.path.append(os.path.abspath(".."))
 from libkeepass.crypto import sha256, transform_key, aes_cbc_decrypt, xor, pad
 from libkeepass.crypto import AES_BLOCK_SIZE
 
+
 class TextCrypto(unittest.TestCase):
     def test_sha256(self):
         self.assertEquals(sha256(b''),
-            b"\xe3\xb0\xc4B\x98\xfc\x1c\x14\x9a\xfb\xf4\xc8\x99o\xb9$'\xaeA"
-            b"\xe4d\x9b\x93L\xa4\x95\x99\x1bxR\xb8U")
+                          b"\xe3\xb0\xc4B\x98\xfc\x1c\x14\x9a\xfb\xf4\xc8\x99o\xb9$'\xaeA"
+                          b"\xe4d\x9b\x93L\xa4\x95\x99\x1bxR\xb8U")
         self.assertEquals(len(sha256(b'')), 32)
         self.assertEquals(len(sha256(b'asdf')), 32)
 
     def test_transform_key(self):
         self.assertEquals(transform_key(sha256(b'a'), sha256(b'b'), 1),
-            b'"$\xe6\x83\xb7\xbf\xa9|\x82W\x01J\xce=\xaa\x8d{\x18\x99|0\x1f'
-            b'\xbbLT4"F\x83\xd0\xc8\xf9')
+                          b'"$\xe6\x83\xb7\xbf\xa9|\x82W\x01J\xce=\xaa\x8d{\x18\x99|0\x1f'
+                          b'\xbbLT4"F\x83\xd0\xc8\xf9')
         self.assertEquals(transform_key(sha256(b'a'), sha256(b'b'), 2000),
-            b'@\xe5Y\x98\xf7\x97$\x0b\x91!\xbefX\xe8\xb6\xbb\t\xefX>\xb3E\x85'
-            b'\xedz\x15\x9c\x96\x03K\x8a\xa1')
+                          b'@\xe5Y\x98\xf7\x97$\x0b\x91!\xbefX\xe8\xb6\xbb\t\xefX>\xb3E\x85'
+                          b'\xedz\x15\x9c\x96\x03K\x8a\xa1')
 
     def test_aes_cbc_decrypt(self):
         self.assertEquals(aes_cbc_decrypt('datamustbe16byte', sha256(b'b'),
-            'ivmustbe16bytesl'),
-            b'x]\xb5\xa6\xe3\x10\xf4\x88\x91_\x03\xc6\xb9\xfb`)')
+                                          'ivmustbe16bytesl'),
+                          b'x]\xb5\xa6\xe3\x10\xf4\x88\x91_\x03\xc6\xb9\xfb`)')
         self.assertEquals(aes_cbc_decrypt('datamustbe16byte', sha256(b'c'),
-            'ivmustbe16bytesl'),
-            b'\x06\x91 \xd9\\\xd8\x14\xa0\xdc\xd7\x82\xa0\x92\xfb\xe8l')
+                                          'ivmustbe16bytesl'),
+                          b'\x06\x91 \xd9\\\xd8\x14\xa0\xdc\xd7\x82\xa0\x92\xfb\xe8l')
 
     def test_xor(self):
         self.assertEquals(xor(b'', b''), b'')
@@ -44,37 +49,36 @@ class TextCrypto(unittest.TestCase):
 
     def test_pad(self):
         self.assertEquals(pad(b''),
-            b'\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10')
+                          b'\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10')
         self.assertEquals(pad(b'\xff'),
-            b'\xff\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f')
+                          b'\xff\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f')
         self.assertEquals(pad(b'\xff\xff'),
-            b'\xff\xff\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e')
+                          b'\xff\xff\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e')
         self.assertEquals(pad(b'\xff\xff\xff'),
-            b'\xff\xff\xff\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d')
+                          b'\xff\xff\xff\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d')
         self.assertEquals(pad(b'\xff\xff\xff\xff'),
-            b'\xff\xff\xff\xff\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c')
+                          b'\xff\xff\xff\xff\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c')
         self.assertEquals(pad(b'\xff\xff\xff\xff\xff'),
-            b'\xff\xff\xff\xff\xff\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b')
+                          b'\xff\xff\xff\xff\xff\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b')
         self.assertEquals(len(pad(b'\xff')), AES_BLOCK_SIZE)
-        self.assertEquals(len(pad(b'\xff'*0)), AES_BLOCK_SIZE)
-        self.assertEquals(len(pad(b'\xff'*1)), AES_BLOCK_SIZE)
-        self.assertEquals(len(pad(b'\xff'*2)), AES_BLOCK_SIZE)
-        self.assertEquals(len(pad(b'\xff'*15)), AES_BLOCK_SIZE)
-        self.assertEquals(len(pad(b'\xff'*16)), 2*AES_BLOCK_SIZE)
-        self.assertEquals(len(pad(b'\xff'*17)), 2*AES_BLOCK_SIZE)
+        self.assertEquals(len(pad(b'\xff' * 0)), AES_BLOCK_SIZE)
+        self.assertEquals(len(pad(b'\xff' * 1)), AES_BLOCK_SIZE)
+        self.assertEquals(len(pad(b'\xff' * 2)), AES_BLOCK_SIZE)
+        self.assertEquals(len(pad(b'\xff' * 15)), AES_BLOCK_SIZE)
+        self.assertEquals(len(pad(b'\xff' * 16)), 2 * AES_BLOCK_SIZE)
+        self.assertEquals(len(pad(b'\xff' * 17)), 2 * AES_BLOCK_SIZE)
 
 
 class TestModule(unittest.TestCase):
-
     def test_get_kdb_class(self):
         # v3
         self.assertIsNotNone(libkeepass.get_kdb_reader([0x9AA2D903, 0xB54BFB65]))
         self.assertEquals(libkeepass.get_kdb_reader([0x9AA2D903, 0xB54BFB65]),
-            libkeepass.kdb3.KDB3Reader)
+                          libkeepass.kdb3.KDB3Reader)
         # v4
         self.assertIsNotNone(libkeepass.get_kdb_reader([0x9AA2D903, 0xB54BFB67]))
         self.assertEquals(libkeepass.get_kdb_reader([0x9AA2D903, 0xB54BFB67]),
-            libkeepass.kdb4.KDB4Reader)
+                          libkeepass.kdb4.KDB4Reader)
 
         # mythical pre2.x signature
         with self.assertRaisesRegexp(IOError, "Unknown sub signature."):
@@ -92,7 +96,6 @@ class TestModule(unittest.TestCase):
 
 
 class TestCommon(unittest.TestCase):
-
     def test_header_dict(self):
         h = libkeepass.common.HeaderDictionary()
         # configure fields
@@ -178,13 +181,13 @@ class TestCommon(unittest.TestCase):
         h.hash = b'\x91.\xc8\x03\xb2\xceI\xe4\xa5A\x06\x8dIZ'
         self.assertEquals(h.hash, b'\x91.\xc8\x03\xb2\xceI\xe4\xa5A\x06\x8dIZ')
         self.assertEquals(h.b.hash, b'\x91.\xc8\x03\xb2\xceI\xe4\xa5A\x06\x8dIZ')
-        #assert False
+        # assert False
 
 # created with KeePassX 0.4.3
 absfile2 = os.path.abspath('tests/sample7_kpx.kdb')
 # created with KeePass 2.19 on linux
 filename1 = 'sample1.kdbx'
-absfile1 = os.path.abspath('tests/'+filename1)
+absfile1 = os.path.abspath('tests/' + filename1)
 absfile3 = os.path.abspath('tests/sample2.kdbx')
 keyfile3 = os.path.abspath('tests/sample2_keyfile.key')
 absfile4 = os.path.abspath('tests/sample3.kdbx')
@@ -197,7 +200,6 @@ output4 = os.path.abspath('tests/output4.kdbx')
 
 
 class TestKDB4(unittest.TestCase):
-
     def test_class_interface(self):
         """Test direct KDB4Reader class usage"""
         kdb = libkeepass.kdb4.KDB4Reader()
@@ -218,7 +220,7 @@ class TestKDB4(unittest.TestCase):
             self.assertEquals(kdb.opened, True)
             self.assertEquals(kdb.read(32), b'<?xml version="1.0" encoding="ut')
             kdb.set_compression(0)
-            #kdb.set_comment("this is pretty cool!")
+            # kdb.set_comment("this is pretty cool!")
             kdb.clear_credentials()
             kdb.add_credentials(password="yxcv")
             with open(output1, 'wb') as outfile:
@@ -302,7 +304,7 @@ class TestKDB4(unittest.TestCase):
 
             self.assertIsNotNone(kdb.pretty_print())
 
-#        # valid password and plain keyfile, uncompressed kdb
+# # valid password and plain keyfile, uncompressed kdb
 #        with libkeepass.open(absfile5, password="qwer", keyfile=keyfile5) as kdb:
 #            self.assertIsNotNone(kdb)
 #            self.assertIsInstance(kdb, libkeepass.kdb4.KDB4Reader)
