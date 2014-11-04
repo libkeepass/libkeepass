7a9e9d327c1ae0e5ab548a92ca8752ca2039098b
George Schizas <gschizas@gmail.com>
2014-11-04 10:26:39 +0200

    PEP8 Formatting

--- libkeepass/crypto.py
+++ libkeepass/crypto.py
@@ -6,9 +6,11 @@ from libkeepass.pureSalsa20 import Salsa20
 
 AES_BLOCK_SIZE = 16
 
+
 def sha256(s):
     """Return SHA256 digest of the string `s`."""
     return bytes(hashlib.sha256(s).digest())
+
 
 def transform_key(key, seed, rounds):
     """Transform `key` with `seed` `rounds` times using AES ECB."""
@@ -20,21 +22,26 @@ def transform_key(key, seed, rounds):
     # return hash of transformed key
     return sha256(key)
 
+
 def aes_cbc_decrypt(data, key, enc_iv):
     """Decrypt and return `data` with AES CBC."""
     cipher = AES.new(key, AES.MODE_CBC, enc_iv)
     return cipher.decrypt(data)
 
+
 def aes_cbc_encrypt(data, key, enc_iv):
     cipher = AES.new(key, AES.MODE_CBC, enc_iv)
     return cipher.encrypt(data)
 
+
 def unpad(data):
-    return data[:len(data)-bytearray(data)[-1]]
+    return data[:len(data) - bytearray(data)[-1]]
+
 
 def pad(s):
     n = AES_BLOCK_SIZE - len(s) % AES_BLOCK_SIZE
     return s + n * struct.pack('b', n)
+
 
 def xor(aa, bb):
     """Return a bytearray of a bytewise XOR of `aa` and `bb`."""
