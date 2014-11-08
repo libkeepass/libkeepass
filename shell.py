283a28d0ef63e853cdd625fa66e345775afcecec
George Schizas <gschizas@gmail.com>
2014-11-08 23:24:25 +0200

    Renamed cmd to shell to avoid conflict with built-in module

--- shell.py
+++ shell.py
@@ -9,22 +9,27 @@ import os
 
 import cmd
 
+
 class KeePassShell(cmd.Cmd):
-    intro = 'Welcome to KeePassCmd. Type "open" to open a file'
+    intro = 'Welcome to KeePassShell. Type "open" to open a file'
     prompt = 'keepass>'
     root = None
     tree = None
 
     def do_open(self, arg):
-        "Open a file"
+        """Open a file"""
         pwd = getpass.getpass()
-        with libkeepass.open(os.path.expanduser(arg), password=pwd) as kdb:
-            kdbx_data = kdb.pretty_print()
-            self.root = lxml.etree.fromstring(kdbx_data)
-            self.tree = lxml.etree.ElementTree(self.root)
+        try:
+            with libkeepass.open(os.path.expanduser(arg), password=pwd) as kdb:
+                kdbx_data = kdb.pretty_print()
+                self.root = lxml.etree.fromstring(kdbx_data)
+                self.tree = lxml.etree.ElementTree(self.root)
+        except OSError as ex:
+            print(ex)
+
 
     def do_search(self, arg):
-        "Search in a file"
+        """Search in a file"""
         if self.root is None or self.tree is None:
             print("You must open a file first")
             return
@@ -34,15 +39,15 @@ class KeePassShell(cmd.Cmd):
             "Entry/String["
             "(Key='Title' and re:test(Value, '{0}', 'i')) or "
             "(Key='URL' and contains(Value,'{0}'))]/..").format(search_term.replace("'", "\\'"))
-        print(xpath_query)
+        # print(xpath_query)
         for e in self.root.xpath(xpath_query, namespaces={"re": "http://exslt.org/regular-expressions"}):
             print()
             groups_path = [p.find(".//Name").text for p in e.iterancestors() if p.tag == 'Group']
             print('/'.join(groups_path[::-1]))
-            #print(tree.getpath(e))
-            #print(lxml.etree.tostring(e).decode())
-            #print(lxml.etree.tostring(e.find('.//String[Key="URL"]')).decode())
-            #print(lxml.etree.tostring(e.find('.//String[Key="Password"]')))
+            # print(tree.getpath(e))
+            # print(lxml.etree.tostring(e).decode())
+            # print(lxml.etree.tostring(e.find('.//String[Key="URL"]')).decode())
+            # print(lxml.etree.tostring(e.find('.//String[Key="Password"]')))
             title = e.find('.//String[Key="Title"]/Value')
             if title is not None:
                 title = title.text
@@ -54,6 +59,7 @@ class KeePassShell(cmd.Cmd):
             print('{}:\t{} {} ({})'.format(title, username, password, url))
 
     def do_bye(self, arg):
+        """Exit the Keepass Shell"""
         return True
 
 
