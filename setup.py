02c30bee129dc2a829e8b17caffbe3374ce108d5
Nikolai Amelichev <entropia@yandex-team.ru>
2016-02-17 01:40:59 +0300

    Update version info

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = "libkeepass",
-    version = "0.1.2",
+    version = "0.1.3",
     packages = ["libkeepass"],
-    author = "Lukas Koell",
-    author_email = "phpwutz@gmail.com",
+    author = "Nikolay Amelichev",
+    author_email = "nickamelichev@gmail.com",
     description = "A library to access keepass v3 (keepass1) and v4 (keepass2) files",
     license = "GPL",
     keywords = "keepass library",
-    url = "https://github.com/phpwutz/libkeepass",   # project home page, if any
+    url = "https://github.com/nvamelichev/libkeepass",   # project home page, if any
     install_requires=["lxml==3.2.1", "nose==1.3.0", "pycrypto==2.6.1"],
     test_suite="tests"
 )
