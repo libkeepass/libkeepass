fc6ed019ba723893b325e846a7ccb775915bfd56
Nikolai Amelichev <entropia@yandex-team.ru>
2016-02-17 02:53:25 +0300

    Version 0.1.5, now supports saving KeePass 2.x files, not only reading them

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "libkeepass",
-    version = "0.1.4",
+    version = "0.1.5",
     packages = ["libkeepass"],
     author = "Nikolay Amelichev",
     author_email = "nickamelichev@gmail.com",
