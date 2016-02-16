8722d55e40ca7e0725959bc5c23298a010ad6436
Nikolai Amelichev <entropia@yandex-team.ru>
2016-02-17 01:45:35 +0300

    FIX: added KeePass exception handling to samples/query.py

--- samples/query.py
+++ samples/query.py
@@ -10,20 +10,20 @@ except IndexError:
     print('query.py <kdbx file name> <entry title>')
     sys.exit(1)
 
-#try:
-with libkeepass.open(filename, password=getpass.getpass()) as kdb:
-    found = {}
-    for entry in kdb.obj_root.findall('.//Group/Entry'):
-        uuid = entry.find('./UUID').text
-        kv = {string.find('./Key').text : string.find('./Value').text for string in entry.findall('./String')}
-        if kv['Title'] == entry_title:
-            found[uuid] = kv['Password']
+try:
+    with libkeepass.open(filename, password=getpass.getpass()) as kdb:
+        found = {}
+        for entry in kdb.obj_root.findall('.//Group/Entry'):
+            uuid = entry.find('./UUID').text
+            kv = {string.find('./Key').text : string.find('./Value').text for string in entry.findall('./String')}
+            if kv['Title'] == entry_title:
+                found[uuid] = kv['Password']
 
-    removed_uuids = {uuid.text for uuid in kdb.obj_root.findall('.//DeletedObject/UUID')}
+        removed_uuids = {uuid.text for uuid in kdb.obj_root.findall('.//DeletedObject/UUID')}
 
-for password in { found[k] for k in found.keys() if k not in removed_uuids }:
-    print(password)
-#except Exception as e:
-#    print('Could not query KeePass Database %s:\n%s' % (filename, str(e)), file=sys.stderr)
-#    sys.exit(2)
+    for password in { found[k] for k in found.keys() if k not in removed_uuids }:
+        print(password)
+except Exception as e:
+    print('Could not query KeePass Database %s:\n%s' % (filename, str(e)), file=sys.stderr)
+    sys.exit(2)
 
