4dd0375a83fbc125f0736dfa04e4ba75963fd83b
Nikolai Amelichev <entropia@yandex-team.ru>
2016-02-17 01:27:01 +0300

    Add usage samples

---
+++ samples/prettyprint.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import libkeepass
+import getpass
+import sys
+
+try:
+    filename = sys.argv[1]
+    password = sys.argv[2] if len(sys.argv) == 3 else getpass.getpass()
+except IndexError:
+    print('prettyprint.py <kdbx file name> [password]')
+    sys.exit(1)
+
+try:
+    with libkeepass.open(filename, password=password) as kdb:
+        print(kdb.pretty_print().decode('unicode_escape'))
+except Exception as e:
+    print('Could not prettyprint KeePass Database %s:\n%s' % (filename, str(e)), file=sys.stderr)
+    sys.exit(2)
+
