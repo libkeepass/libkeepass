2a95765a89588103afa0522560470777bdb345a0
Glenn Washburn <development@efficientek.com>
2016-06-13 22:48:54 -0500

    Add tests for twofish encryption on KDB4 files only.

--- tests/tests.py
+++ tests/tests.py
@@ -12,7 +12,8 @@ import libkeepass.kdb3
 sys.path.append(os.path.abspath("."))
 sys.path.append(os.path.abspath(".."))
 
-from libkeepass.crypto import sha256, transform_key, aes_cbc_decrypt, xor, pad
+from libkeepass.crypto import sha256, transform_key, xor, pad
+from libkeepass.crypto import aes_cbc_decrypt, twofish_cbc_decrypt, twofish_cbc_encrypt
 from libkeepass.crypto import AES_BLOCK_SIZE
 
 
@@ -39,6 +40,17 @@ class TextCrypto(unittest.TestCase):
         self.assertEquals(aes_cbc_decrypt(b'datamustbe16byte', sha256(b'c'),
                                           b'ivmustbe16bytesl'),
                           b'\x06\x91 \xd9\\\xd8\x14\xa0\xdc\xd7\x82\xa0\x92\xfb\xe8l')
+
+    def test_twofish_cbc_decrypt(self):
+        self.assertEquals(twofish_cbc_encrypt(b'datamustbe16byte', sha256(b'b'),
+                                              b'ivmustbe16bytesl'),
+                          b'\xd4^&`\xe1j\xfd{\xeb\x04{\x90f\xed\xebi')
+        self.assertEquals(twofish_cbc_encrypt(b'datamustbe16byte', sha256(b'c'),
+                                              b'ivmustbe16bytesl'),
+                          b'\xb3\x86\xbe\x0ficZW\x92P\xeb\x17\xa8\xa8\xac\xe8')
+        self.assertEquals(twofish_cbc_decrypt(b'K\x07q\xad\xd0\x8d\xb9\x0f\x15\xef\x87\x089\xc3\x83\x9c',
+                                              sha256(b'd'), b'ivmustbe16bytesl'),
+                          b'datamustbe16byte')
 
     def test_xor(self):
         self.assertEquals(xor(b'', b''), b'')
@@ -185,6 +197,8 @@ absfile4 = os.path.abspath('tests/sample3.kdbx')
 keyfile4 = os.path.abspath('tests/sample3_keyfile.exe')
 absfile5 = os.path.abspath('tests/sample4.kdbx')
 keyfile5 = os.path.abspath('tests/sample3_keyfile.exe')
+# created with KeePass 2.32 on linux encrypted with twofish
+absfile6 = os.path.abspath('tests/sample8_twofish.kdbx')
 
 output1 = os.path.abspath('tests/output1.kdbx')
 output4 = os.path.abspath('tests/output4.kdbx')
@@ -294,6 +308,11 @@ class TestKDB4(unittest.TestCase):
             self.assertEquals(xml1, xml4)
 
             self.assertIsNotNone(kdb.pretty_print())
+
+        # twofish encryption
+        with libkeepass.open(absfile6, password="qwerty") as kdb:
+            self.assertIsNotNone(kdb)
+            self.assertEquals(kdb.opened, True)
 
 # # valid password and plain keyfile, uncompressed kdb
 # with libkeepass.open(absfile5, password="qwer", keyfile=keyfile5) as kdb:
