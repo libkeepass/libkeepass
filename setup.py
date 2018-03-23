ab164a0a1e29451eced39975611ef0387eb97052
evan <evan@evanw.org>
2018-03-23 00:48:03 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="libkeepass",
-    version="0.2.0",
+    version="0.3.0",
     packages=["libkeepass"],
     author="Lukas Koell",
     author_email="phpwutz@gmail.com",
