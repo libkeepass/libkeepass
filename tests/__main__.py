df49d61667115fb26ea23cf47c782cb2e58227de
Glenn Washburn <development@efficientek.com>
2018-05-24 14:59:37 -0500

    Add __main__.py to tests module to allow calling as "python tests".

---
+++ tests/__main__.py
@@ -0,0 +1,13 @@
+
+import os
+import sys
+import unittest
+
+testdir = os.path.dirname(__file__)
+projectdir = os.path.dirname(testdir)
+sys.path.insert(0, os.path.abspath(projectdir))
+
+from tests.tests import *
+
+if __name__ == '__main__':
+    unittest.main()
